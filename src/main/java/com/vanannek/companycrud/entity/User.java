15f524357bc9db93d03147ac1f805530b5db0601
AnhAnNe <94521126+AnhAnNek@users.noreply.github.com>
2023-06-03 10:45:04 +0700

    edit config file

--- src/main/java/com/vanannek/companycrud/entity/User.java
+++ src/main/java/com/vanannek/companycrud/entity/User.java
@@ -3,12 +3,14 @@ package com.vanannek.companycrud.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @Entity
+@Table(name = "users")
 @NoArgsConstructor
 @AllArgsConstructor
 public class User {
