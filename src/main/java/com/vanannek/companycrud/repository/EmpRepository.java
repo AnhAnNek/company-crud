319ead7c73fd2ea800a7548d42dd7f326f3ee73c
AnhAnNe <94521126+AnhAnNek@users.noreply.github.com>
2023-06-09 08:15:05 +0700

    add @Repository annotation

--- src/main/java/com/vanannek/companycrud/repository/EmpRepository.java
+++ src/main/java/com/vanannek/companycrud/repository/EmpRepository.java
@@ -2,6 +2,8 @@ package com.vanannek.companycrud.repository;
 
 import com.vanannek.companycrud.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EmpRepository extends JpaRepository<Employee, Long> {
 }
--- src/main/java/com/vanannek/companycrud/repository/ProjectRepository.java
+++ src/main/java/com/vanannek/companycrud/repository/ProjectRepository.java
@@ -4,9 +4,11 @@ import com.vanannek.companycrud.entity.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ProjectRepository extends JpaRepository<Project, Integer> {
     @Query(value = "SELECT * FROM project WHERE owner_id=:ownerId", nativeQuery = true)
     List<Project> findByOwnerId(@Param("ownerId") Long ownerId);
